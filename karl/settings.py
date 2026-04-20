f92726f4e2b5bf8304f0772d2ff0df5fe35d5220
Eugene <eugeneowino62@gmail.com>
2026-04-20 22:17:08 +0300

    runtime.txt

--- karl/settings.py
+++ karl/settings.py
@@ -12,6 +12,18 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+ALLOWED_HOSTS = ['swiftdoc.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
