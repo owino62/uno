8ddbb8d67c0a4f8755495ece0ce4224c03ed3c70
Eugene <eugeneowino62@gmail.com>
2026-05-18 11:58:17 +0300

    edit of manifest in settings.py

--- karl/settings.py
+++ karl/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[BASE_DIR/'static']
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.storage.CompressedStaticFilesStorage'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
