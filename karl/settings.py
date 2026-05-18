f2a70fbb5a043a786ce6793d0b53f4ee67fa54f9
Eugene <eugeneowino62@gmail.com>
2026-05-18 11:51:52 +0300

    changes to base.html

--- karl/settings.py
+++ karl/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[BASE_DIR/'static']
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
